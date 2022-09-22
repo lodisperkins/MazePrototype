bac1c6836f9e9c634fdaeca2f7b0ad9e1120acc9
Lodis <lodisperkins44@gmail.com>
2022-09-22 16:41:10 -0500

    Added a test player controller to the project

---
+++ Assets/Scripts/Movement/PlayerCameraMovementBehaviour.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraMovementBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _height;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PlayerSpawnerBehaviour.Player.transform.position + Vector3.up * _height;
+    }
+}
